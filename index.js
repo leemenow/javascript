898d61a6ffe49ad5fe918870a1a7ce45011aac3c
leemenow <ptwo82015@gmail.com>
2023-06-14 12:26:36 +0100

    gh

---
+++ index.js
@@ -0,0 +1,4 @@
+let Nigeria = "Government had said it would remove fuel subsidy next year and replace it with an N5,000 monthly transport grant to 40 million Nigerians."
+
+console.log(Nigeria);
+let myame = "ghghh";
